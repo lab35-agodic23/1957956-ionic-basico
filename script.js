d8edd23bceeb7baaa61aabc8750b64e5103ecd3f
Vart00325 <york.lalito24@outlook.com>
2023-09-03 22:28:12 -0500

    practica 3

--- script.js
+++ script.js
@@ -1,16 +1,83 @@
-var miPrimerVariable ='hola, mundo'
+const personas = [
+    { usuario: "lalo", contrasena: "12345"},
+    { usuario: "mike", contrasena: "mike" },
+];
 
-var campoTexto = document.getElementById('mitexto1')
+function mostrarSpinner() {
+    document.getElementById("spinner").style.display = "inline-block";
+    document.getElementById("loginButton").disabled = true;
+    document.getElementById("registerButton").disabled = true;
 
-
-function mostrarAlerta(){
-    alert(campoTexto.value);
+    document.body.classList.add("blur-background");
 }
 
-function limpiarTexto(){
+function ocultarSpinner() {
+    document.getElementById("spinner").style.display = "none";
+    document.getElementById("loginButton").disabled = false;
+    document.getElementById("registerButton").disabled = false;
 
-    document.getElementById("mitexto1").value = "";
-
+    document.body.classList.remove("blur-background");
 }
 
-   
+function iniciarSesion() {
+    const usuarioInput = document.getElementById("usuarioInput").value;
+    const contrasenaInput = document.getElementById("contrasenaInput").value;
+
+    if (!usuarioInput || !contrasenaInput) {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
+
+    mostrarSpinner(); // Mostrar el spinner durante la autenticación
+
+    setTimeout(() => {
+        const usuarioEncontrado = personas.find(
+            (persona) => persona.usuario === usuarioInput && persona.contrasena === contrasenaInput
+        );
+
+        if (usuarioEncontrado) {
+            alert(`Inicio de sesión exitoso. Bienvenido, ${usuarioEncontrado.usuario}!`);
+
+            console.log("Genial");
+            console.log(personas);
+        } else {
+            alert("Credenciales incorrectas. Intente de nuevo.");
+            console.log("No genial");
+        }
+
+        ocultarSpinner(); // Ocultar el spinner después de la autenticación
+        limpiarCampos();
+
+    }, 2000); // Simulación de inicio de sesión de 2 segundos
+}
+
+function registrarUsuario() {
+    const usuarioInput = document.getElementById("usuarioInput").value;
+    const contrasenaInput = document.getElementById("contrasenaInput").value;
+
+    if (!usuarioInput || !contrasenaInput) {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
+
+    mostrarSpinner(); // Mostrar el spinner durante el registro
+
+    setTimeout(() => {
+        const usuarioExistente = personas.some((persona) => persona.usuario === usuarioInput);
+
+        if (usuarioExistente) {
+            alert(`El usuario ya existe. Inicie sesión en lugar de registrarse.`);
+        } else {
+            personas.push({ usuario: usuarioInput, contrasena: contrasenaInput });
+            alert("Registro exitoso. Ahora puede iniciar sesión.");
+        }
+
+        ocultarSpinner(); // Ocultar el spinner después del registro
+        limpiarCampos();
+
+    }, 2000); // Simulación de registro de 2 segundos
+}
+function limpiarCampos() {
+    document.getElementById("usuarioInput").value = "";
+    document.getElementById("contrasenaInput").value = "";
+}
